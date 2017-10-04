bdd20409201e79aec0e4031c8b934e065a2d936b
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-10-04 18:45:33 +0000

    initial commit and commented out starter assert test

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -13,8 +13,8 @@ class PiezasTest : public ::testing::Test
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
-
+/*
 TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
-}
+}*/
