e3fbd4a26c947c61175cfb8cb28247b6d0b81384
Dr Kevin Buffardi <kbuffardi@csuchico.edu>
2017-10-07 05:42:49 +0000

    progress in gamestate funtion

--- Piezas.cpp
+++ Piezas.cpp
@@ -74,7 +74,7 @@ Piece Piezas::dropPiece(int column)
 **/
 Piece Piezas::pieceAt(int row, int column)
 {
-    if (row > BOARD_ROWS || row < 0 || column > BOARD_COLS || column < 0) { //out of bounds
+    if (row >= BOARD_ROWS || row < 0 || column >= BOARD_COLS || column < 0) { //out of bounds
         return Invalid;
     }
     return board[row][column];
@@ -89,7 +89,60 @@ Piece Piezas::pieceAt(int row, int column)
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
 **/
+
+/**
+ * [2,0][2,1][2,2][2,3]
+ * [1,0][1,1][1,2][1,3]
+ * [0,0][0,1][0,2][0,3]
+**/
 Piece Piezas::gameState()
 {
+    //invalid if board is not full - return invalid
+    if (pieceAt(2,0) == Blank || pieceAt(2,1) == Blank || pieceAt(2,2) == Blank || pieceAt(2,3) == Blank) { //Top row is not full thus gameboard is not full
+        return Invalid;
+    } 
+    //winner - return winner
+    int xBiggestStreak = 0;
+    int oBiggestStreak = 0;
+    //rows
+    for (int i = 0; i > BOARD_ROWS; i++) {
+        if (pieceAt(i,0) == pieceAt(i,1) && pieceAt(i,1) == pieceAt(i,2) && pieceAt(i,2) == pieceAt(i,3)) {//4 streak
+            if (pieceAt(i,0) == X) {
+                xBiggestStreak = 4;
+            } else if (pieceAt(i,0) == O) {
+                oBiggestStreak = 4;
+            }
+        } else if ( //3 streak
+            (pieceAt(i,0) == pieceAt(i,1) && pieceAt(i,1) == pieceAt(i,2)) || //left 3 columns
+            (pieceAt(i,1) == pieceAt(i,2) && pieceAt(i,2) == pieceAt(i,3))  //right 3 columns
+        ) {
+            if (pieceAt(i,0) == X) {
+                xBiggestStreak = 3;
+            } else if (pieceAt(i,0) == O) {
+                oBiggestStreak = 3;
+            }
+        } else if ( //2 streak
+            (pieceAt(i, 0) == pieceAt(i, 1)) ||   //first columns
+            (pieceAt(i, 1) == pieceAt(i, 2)) ||   //middle columns
+            (pieceAt(i, 2) == pieceAt(i, 3))      //last 2 columns
+        ) {
+                return X;
+        }
+    }
+    
+    //columns
+    if (xBiggestStreak > oBiggestStreak) {
+        return X;
+    } else if (xBiggestStreak < oBiggestStreak) {
+        return O;
+    } else { 
+        return Blank;
+    }
+    /**
+    for (int i = 0; i < BOARD_ROWS; i++) {
+        for (int j = 0; j < BOARD_COLS; j++) {
+        }
+    }**/
+    //tie no one has won - return blank
     return Blank;
 }
